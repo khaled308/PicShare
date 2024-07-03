7cebc1ef94e7f89365244795eebe51d10e2d0b1d
khaled308 <khaledgaballa4@gmail.com>
2024-07-03 07:02:03 +0300

    api docs

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,9 +1,16 @@
+import path from "path";
+import { fileURLToPath } from "url";
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
+import yaml from "yamljs";
 import { PORT } from "./utils/constants.js";
 import imageRoutes from "./routes/image.routes.js";
 import DBConnection from "./utils/db.js";
+
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -12,7 +19,10 @@ app.use(morgan("dev"));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/image", imageRoutes);
+app.use("/api/images", imageRoutes);
+
+const swaggerDocument = yaml.load(path.join(__dirname, "../swagger.yaml"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((error, req, res, next) => {
   res.status(error.status || 500);
